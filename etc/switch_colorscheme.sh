f04cbea9e459ea80a2b818568445e80418498fb2
Lukas Osterloh <lukas.osterloh@gmail.com>
2024-12-27 11:26:49 +0100

    Use ghostty instead of kitty

--- etc/switch_colorscheme.sh
+++ etc/switch_colorscheme.sh
@@ -2,10 +2,12 @@
 
 if [ -f ~/.dark ]; then
     ln -Fs ~/.config/kitty/theme_light.conf ~/.config/kitty/theme.conf && kill -SIGUSR1 $(ps -e | grep kitty | head -1 | xargs | cut -d' ' -f1)
+    sed -i -E 's/theme = catppuccin-frappe/theme = catppuccin-latte/' "$HOME/Library/Application Support/com.mitchellh.ghostty/config"
     rm ~/.dark
     touch ~/.light
 else
     ln -Fs ~/.config/kitty/theme_dark.conf ~/.config/kitty/theme.conf && kill -SIGUSR1 $(ps -e | grep kitty | head -1 | xargs | cut -d' ' -f1)
+    sed -i -E 's/theme = catppuccin-latte/theme = catppuccin-frappe/' "$HOME/Library/Application Support/com.mitchellh.ghostty/config"
     rm ~/.light
     touch ~/.dark
 fi
