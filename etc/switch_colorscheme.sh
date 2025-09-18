f9a9a58b7e8bad41bfe70c5c5d541fbb9ce1e5f9
Lukas Osterloh <lukas.osterloh@gmail.com>
2025-09-18 11:33:08 +0200

    Fix themes after Ghostty updates

--- etc/switch_colorscheme.sh
+++ etc/switch_colorscheme.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ -f ~/.dark ]; then
-    sed -i -E 's/theme = catppuccin-frappe/theme = catppuccin-latte/' "$HOME/Library/Application Support/com.mitchellh.ghostty/config"
+    sed -i -E 's/theme = "Catppuccin Frappe"/theme = "Catppuccin Latte"/' "$HOME/Library/Application Support/com.mitchellh.ghostty/config"
     rm ~/.dark
     touch ~/.light
 else
-    sed -i -E 's/theme = catppuccin-latte/theme = catppuccin-frappe/' "$HOME/Library/Application Support/com.mitchellh.ghostty/config"
+    sed -i -E 's/theme = "Catppuccin Latte"/theme = "Catppuccin Frappe"/' "$HOME/Library/Application Support/com.mitchellh.ghostty/config"
     rm ~/.light
     touch ~/.dark
 fi
